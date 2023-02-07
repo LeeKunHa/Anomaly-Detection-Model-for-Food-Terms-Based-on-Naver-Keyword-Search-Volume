fa30b20d41c277614ef2c3054461272731f9559b
LeeKunHa <kunha98@gmail.com>
2023-02-07 16:23:30 +0900

    merge Absolute_Value.py

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
 import csv
 
+from Relative_Value import *
+from Absolute_Value import *
 from config import *
 _cfg = config
 
-if __name__ == "__main__":
-    # 검색어 선택
-    # 자체 검색어
-    keyword_list = _cfg['Keyword']['User_list']
-    # 식신 카테고리
-    keyword_list = _cfg['Keyword']['Siksin']
-    # 식품첨가물용어집(식품안전나라 용어사전)
-    keyword_list = _cfg['Keyword']['Food_Safety_Dict']
-    # 네이버 식품 카테고리 인기검색어 top500
-    keyword_list = _cfg['Keyword']['Naver_Food_Rank']
-    # 요식업 브랜드명
-    keyword_list = _cfg['Keyword']['Food_Brand']
-    # 식품안전정보 검색어(식품안전정보원 글로벌 정보부)
-    keyword_list = _cfg['Keyword']['Food_Safety_Issue']
-    # 식품안전나라 검색어 기록(22/09/13 기준)
-    keyword_list = _cfg['Keyword']['Food_Safety_Search']
 
-    # 검색어 통합
+if __name__ == "__main__":
+    
+    # 검색어 목록 config에서 불러오기(데이터 비공개 목적으로 config파일에 포함했을 뿐, main파일에 리스트로 바로 작성해도 문제없습니다.)
     keyword_list = sum(_cfg['Keyword'].values(), [])
+    #공백제거(한달치 검색량에 안 잡힘)
+    keyword_list = [x.replace(' ','') for x in keyword_list]
     # 중복제거
     keyword_unique = []
     [keyword_unique.append(x) for x in keyword_list if x not in keyword_unique].clear() #None 반복 출력을 막기 위한 clear
-    keyword_list = [x.replace(' ','') for x in keyword_list] #공백제거(한달치 검색량에 안 잡힘)
+    keyword_list = keyword_unique
 
 
     # 최대값을 갖는 검색어 찾기
+    print('최대값을 갖는 검색어를 찾는 중입니다..')
     total_max_key, single_list, error_list = find_max_key(keyword_list)
     error_list = sum(error_list, [])
     total_max_key, single_list, final_error = find_error_list(error_list, total_max_key, single_list)
     keyword_list = [x for x in keyword_list if x not in final_error]
-    
-    
+    print(f"최대값을 갖는 검색어: {total_max_key}")
+
     # keyword_list 저장(final_error는 제외, single_list는 포함)
     with open('data/input_keyword/total_keyword.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(keyword_list)
-
+    
     # 불러오기(# keyword_list를 바꾸지 않았으면 여기부터!!)
     keyword_list = []
     with open('data/input_keyword/total_keyword.csv','r',newline='') as f:
@@ -49,10 +40,22 @@ if __name__ == "__main__":
     keyword_list = sum(keyword_list, [])
 
     single_list = find_unmax_key(single_list)
+    #검색량 상대값 산출
+    print('검색어 상대값을 산출 중입니다..')
     df = search_amount(keyword_list,total_max_key,single_list)
     #검색량 상대값 저장
     date = str(datetime.now().date().strftime('%Y%m%d'))
-    df.to_csv(f'data/result/search_result{date}.csv', index=False)
-    df = pd.read_csv(f'data/result/search_result{date}.csv')
-    df['날짜'] = df['날짜'].astype('datetime64[ns]')
+    df.to_csv(f'data/result/search_result_relative{date}.csv', index=False)
+    #df = pd.read_csv(f'data/result/search_result_relative{date}.csv')
+    #df['날짜'] = df['날짜'].astype('datetime64[ns]')
 
+
+    #상대값->절대값 변환
+    print('상대값->절대값 변환 작업을 진행 중입니다..')
+    final_df, error_per_average, MSE_average = real_amount(keyword_list,df,total_max_key,single_list)
+    print(f"검색어 5개를 샘플링하여 계산한 오차율: {error_per_average}, MSE: {MSE_average}")
+    #검색량 절대값 저장
+    date = str(datetime.now().date().strftime('%Y%m%d'))
+    final_df.to_csv(f'data/result/search_result_absolute{date}.csv', index=False)
+    #df = pd.read_csv(f'data/result/search_result_absolute{date}.csv')
+    #df['날짜'] = df['날짜'].astype('datetime64[ns]')
